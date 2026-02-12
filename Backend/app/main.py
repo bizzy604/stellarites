c3b131b54e8719a8bca8a86d76c6ade2e37628f5
Amoni Kevin <77682952+bizzy604@users.noreply.github.com>
2026-02-12 12:27:46 +0300

    Add new allowed origins for CORS

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -66,6 +66,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
+        "https://stellarites.vercel.app",
+        "https://stellarites.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
